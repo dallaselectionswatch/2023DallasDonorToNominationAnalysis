485bf26044ffb3a9cd13dbfb7b044817fdef3343
supermercado97 <isaiahmercadotx@gmail.com>
2023-07-11 14:14:11 -0500

    full name comparison and writing to excel

--- committeeToDonorAnalysis.py
+++ committeeToDonorAnalysis.py
@@ -9,13 +9,19 @@ import xlsxwriter
 pdf_path = 'boardmembers.pdf'
 pdf_file = open(pdf_path, 'rb')
 nlp = spacy.load('en_core_web_sm')
-workbook = xlsxwriter.Workbook('last_name_of_donor_and_committee_member_match.xlsx')
-worksheet = workbook.add_worksheet()
+workbook = xlsxwriter.Workbook('donor_committee_member_matches.xlsx')
+last_name_match_worksheet = workbook.add_worksheet("last_name_match")
 #write column headers to worksheet
-worksheet.write(0, 0, "Committee Member")
-worksheet.write(0, 1, "Donor")
-worksheet.write(0, 2, "Amount")
-worksheet.write(0, 3, "Campaign")
+last_name_match_worksheet.write(0, 0, "Committee Member")
+last_name_match_worksheet.write(0, 1, "Donor")
+last_name_match_worksheet.write(0, 2, "Amount")
+last_name_match_worksheet.write(0, 3, "Campaign")
+
+full_name_match_worksheet = workbook.add_worksheet("full_name_match")
+full_name_match_worksheet.write(0, 0, "Committee Member")
+full_name_match_worksheet.write(0, 1, "Donor")
+full_name_match_worksheet.write(0, 2, "Amount")
+full_name_match_worksheet.write(0, 3, "Campaign")
 
 # Create a PDF reader object
 pdf_reader = PyPDF2.PdfReader(pdf_file)
@@ -95,18 +101,26 @@ excel_path = '2023 Dallas Campaign Donors.xlsx'
 df = pd.read_excel(excel_path)
 # Iterate over each donation from excel
 last_name_match_row = 1
+full_name_match_row = 1
 print("Comparing names of committee members to campaign donors")
 for committee_member in extracted_names:
-    member_last_name = committee_member.split()[-2] if isSuffix(committee_member.split()[-1]) else committee_member.split()[-1].lower()
-
+    member_last_name = committee_member.split()[-2].lower() if isSuffix(committee_member.split()[-1]) else committee_member.split()[-1].lower()
+    member_first_name = committee_member.split()[0].lower()
     # Search for matching name of a nominee
     for index, row in df.iterrows():
-        donor_last_name = str(row['Donor']).split(",")[0].lower()  # Convert the cell value to string
+        donor_last_name = str(row['Donor']).split(",")[0].lower()
+        donor_name = str(row['Donor']).split("\n")[0].lower()
         if donor_last_name == member_last_name:
-            worksheet.write(last_name_match_row, 0, committee_member)
-            worksheet.write(last_name_match_row, 1, str(row['Donor']))
-            worksheet.write(last_name_match_row, 2, str(row['Amount']))
-            worksheet.write(last_name_match_row, 3, str(row['Candidate']))
+            last_name_match_worksheet.write(last_name_match_row, 0, committee_member)
+            last_name_match_worksheet.write(last_name_match_row, 1, str(row['Donor']))
+            last_name_match_worksheet.write(last_name_match_row, 2, str(row['Amount']))
+            last_name_match_worksheet.write(last_name_match_row, 3, str(row['Candidate']))
             last_name_match_row = last_name_match_row + 1
+            if member_first_name in donor_name:
+                full_name_match_worksheet.write(full_name_match_row, 0, committee_member)
+                full_name_match_worksheet.write(full_name_match_row, 1, str(row['Donor']))
+                full_name_match_worksheet.write(full_name_match_row, 2, str(row['Amount']))
+                full_name_match_worksheet.write(full_name_match_row, 3, str(row['Candidate']))
+                full_name_match_row = full_name_match_row + 1
 
 workbook.close()
